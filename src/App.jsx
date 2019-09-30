fdec1e842a4f4b6eda7c8f4c4934ade556806d30
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-09-30 08:24:17 -0300

    Adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import GlobalStyle from './styles/global'
 
 import history from './services/history'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <GlobalStyle />
         <Header />
         <Routes />
+        <Footer />
       </Router>
     </>
   )
